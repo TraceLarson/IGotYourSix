06dce6b9a4de9078917f0c2c0473586283963ac5
Trace Larson <trace.larson4917@gmail.com>
2017-11-06 19:52:51 -0600

    JS not working on webserver, troubleshooting

--- js/content.js
+++ js/content.js
@@ -89,7 +89,7 @@ if(window.location.pathname == '/loadouts.html') {
 
 //--------Inventory Page [
 
-// if(window.location.pathname == '/inventory.html') {
+if(window.location.pathname == './inventory.html') {
 
 	const tabcontent = document.getElementsByClassName("tabcontent");
 	const tablinks = document.getElementsByClassName("tablinks");
@@ -131,5 +131,5 @@ if(window.location.pathname == '/loadouts.html') {
 
 
 	}
-// }
+ }
 //----TAB CONTROL ]
