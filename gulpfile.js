329129508e3fcd539d2971df426a677afab822ac
Trace Larson <trace.larson4917@gmail.com>
2017-11-12 19:03:31 -0600

    Trouble shooting Authorization to Destiny api

--- gulpfile.js
+++ gulpfile.js
@@ -2,38 +2,38 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var postcss = require('gulp-postcss');
-//var autoprefixer = require('autoprefixer');
+var autoprefixer = require('autoprefixer');
 
-gulp.task('defualt', function() {
+gulp.task('defualt', function () {
 
 });
 
 
 gulp.task('scss', function () {
-var processors = [
-  autoprefixer({browsers: ['last 2 versions']}),
-];
-return gulp.src('./scss/*.scss')
-.pipe(sass())
-.pipe(postcss(processors))
-.pipe(gulp.dest('./css'))
-.pipe(browserSync.reload({
-stream:true
-}))
+	var processors = [
+		autoprefixer({browsers: ['last 2 versions']}),
+	];
+	return gulp.src('./scss/*.scss')
+		.pipe(sass())
+		.pipe(postcss(processors))
+		.pipe(gulp.dest('./css'))
+		.pipe(browserSync.reload({
+			stream: true
+		}))
 });
 
-gulp.task('browser-sync',function () {
-browserSync.init({
-server:{
-baseDir: "./"
-}
-})
+gulp.task('browser-sync', function () {
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	})
 });
 
-gulp.task('watch',['browser-sync','scss'], function () {
-gulp.watch('./scss/**/*.scss', ['scss']);
-gulp.watch('*.html').on('change', browserSync.reload);
-gulp.watch('*.js').on('change', browserSync.reload);
+gulp.task('watch', ['browser-sync', 'scss'], function () {
+	gulp.watch('./scss/**/*.scss', ['scss']);
+	gulp.watch('*.html').on('change', browserSync.reload);
+	gulp.watch('*.js').on('change', browserSync.reload);
 
 });
 
